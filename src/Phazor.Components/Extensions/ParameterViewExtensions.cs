f5f09f69be048ed6909587e08104a25509e4c4d0
ronimizy <ronimizy@outlook.com>
2023-09-06 19:39:36 +0300

    fix: TryGetUpdatedValue

--- src/Phazor.Components/Extensions/ParameterViewExtensions.cs
+++ src/Phazor.Components/Extensions/ParameterViewExtensions.cs
@@ -5,6 +5,7 @@ namespace Phazor.Components.Extensions;
 
 internal static class ParameterViewExtensions
 {
+    // Reference types
     public static bool TryGetUpdatedValue<T>(
         this ParameterView parameters,
         string parameterName,
@@ -14,10 +15,21 @@ internal static class ParameterViewExtensions
         return parameters.TryGetValue(parameterName, out value) && ReferenceEquals(oldValue, value) is false;
     }
 
+    // Non-nullable value types
     public static bool TryGetUpdatedValue<T>(
         this ParameterView parameters,
         string parameterName,
         T oldValue,
+        [NotNullWhen(true)] out T? value) where T : struct, IEquatable<T>
+    {
+        return parameters.TryGetValue(parameterName, out value) && oldValue.Equals(value) is false;
+    }
+
+    // Nullable value types
+    public static bool TryGetUpdatedValue<T>(
+        this ParameterView parameters,
+        string parameterName,
+        T? oldValue,
         [NotNullWhen(true)] out T? value) where T : struct, IEquatable<T>
     {
         return parameters.TryGetValue(parameterName, out value) && oldValue.Equals(value) is false;
