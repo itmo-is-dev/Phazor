0a0a31a0180a7882efcc9a865a977dc534559c8b
ronimizy <ronimizy@outlook.com>
2026-02-10 21:51:40 +0300

    fix: parameter view extensions

--- src/Phazor.Components/Extensions/ParameterViewExtensions.cs
+++ src/Phazor.Components/Extensions/ParameterViewExtensions.cs
@@ -8,17 +8,16 @@ namespace Phazor.Components.Extensions;
 
 public static class ParameterViewExtensions
 {
-    // Reference types
     public static bool TryGetUpdatedValue<T>(
         this ParameterView parameters,
         string parameterName,
         T oldValue,
         [NotNullWhen(true)] out T? value)
     {
-        return parameters.TryGetValue(parameterName, out value) && ReferenceEquals(oldValue, value) is false;
+        return parameters.TryGetValue(parameterName, out value)
+               && EqualityComparer<T>.Default.Equals(oldValue, value) is false;
     }
 
-    // Reference types (simplified)
     public static bool TryGetUpdatedValue<T>(
         this ParameterView parameters,
         T oldValue,
@@ -27,52 +26,6 @@ public static class ParameterViewExtensions
         string? parameterName = null)
     {
         return parameters.TryGetValue(parameterName ?? string.Empty, out value)
-               && ReferenceEquals(oldValue, value) is false;
-    }
-
-    // Non-nullable value types
-    public static bool TryGetUpdatedValue<T>(
-        this ParameterView parameters,
-        string parameterName,
-        T oldValue,
-        [NotNullWhen(true)] out T? value)
-        where T : struct, IEquatable<T>
-    {
-        return parameters.TryGetValue(parameterName, out value) && oldValue.Equals(value) is false;
-    }
-
-    // Non-nullable value types (simplified)
-    public static bool TryGetUpdatedValue<T>(
-        this ParameterView parameters,
-        T oldValue,
-        [NotNullWhen(true)] out T? value,
-        [CallerArgumentExpression(nameof(oldValue))]
-        string? parameterName = null)
-        where T : struct, IEquatable<T>
-    {
-        return parameters.TryGetValue(parameterName ?? string.Empty, out value) && oldValue.Equals(value) is false;
-    }
-
-    // Nullable value types
-    public static bool TryGetUpdatedValue<T>(
-        this ParameterView parameters,
-        string parameterName,
-        T? oldValue,
-        [NotNullWhen(true)] out T? value)
-        where T : struct, IEquatable<T>
-    {
-        return parameters.TryGetValue(parameterName, out value) && oldValue.Equals(value) is false;
-    }
-
-    // Nullable value types (simplified)
-    public static bool TryGetUpdatedValue<T>(
-        this ParameterView parameters,
-        T? oldValue,
-        [NotNullWhen(true)] out T? value,
-        [CallerArgumentExpression(nameof(oldValue))]
-        string? parameterName = null)
-        where T : struct, IEquatable<T>
-    {
-        return parameters.TryGetValue(parameterName ?? string.Empty, out value) && oldValue.Equals(value) is false;
+               && EqualityComparer<T>.Default.Equals(oldValue, value) is false;
     }
 }
